56757cdf58b467c5f73de488eeaccda823f3467f
Your Name <maliaraees2@gmail.com>
2024-11-23 02:33:43 +0500

    run build

--- src/app/components/home/page.tsx
+++ src/app/components/home/page.tsx
@@ -16,10 +16,10 @@ const Home = () => {
     <>
         <div className='grid sm:grid-cols-1 md:grid-cols-[50%_auto] bg-gray-800 text-white mx-2 md:mx-6 py-5 px-16 border-2 border-cyan-500 rounded md:rounded-none'>
             <div className='mt-20 md:mt-14 md:ml-6 order-2 md:order-1'>
-                <h3 className='font-semibold text-xl'>Hello, It's Me</h3> 
+                <h3 className='font-semibold text-xl'>Hello, It&apos;s Me</h3> 
                 <h1 className='font-bold text-3xl md:text-5xl my-2'>Malia Raees</h1> 
                 <span className='font-semibold text-xl'>
-                    And I'm {""}
+                    And I&apos;m {""}
                 </span>
                 <TypeAnimation 
                 sequence={["Front-end Developer!", 1000, "Back-end Developer!", 1000, 'Responsive Web Designer!', 1000]}
@@ -28,7 +28,7 @@ const Home = () => {
                 speed={30}
                 className='text-cyan-500 text-xl'
                 />
-                <p className='mt-4'>"I'm a web developer with a passion for crafting dynamic, user-friendly websites. I thrive on turning ideas into engaging online experiences."</p>
+                <p className='mt-4'>I&apos;m a web developer with a passion for crafting dynamic, user-friendly websites. I thrive on turning ideas into engaging online experiences.</p>
                 <div className='flex gap-5 mt-4'>
                     <FaFacebook style={{color: 'cyan', fontSize: '20px'}}/> <FaInstagram style={{color: 'cyan', fontSize: '20px'}}/> <FaLinkedin style={{color: 'cyan', fontSize: '20px'}}/> <FaGithub style={{color: 'cyan', fontSize: '20px'}}/>
                 </div>
